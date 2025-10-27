1946802d9f6c053a52dcb6da329785fcb1cd6d75
Muhammad Haikal <2511500011@mahasiswa.atmaluhur.ac.id>
2025-10-27 20:41:23 +0700

    memasukan elemen errormsg

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -6,3 +6,8 @@ document.querySelector("form").addEventListener("submit", function (e) {
         const nama = document.getElementById("txtNama"); 
         const email = document.getElementById("txtEmail"); 
         const pesan = document.getElementById("txtPesan");
+
+        document.querySelectorAll(".error-msg").forEach(el => el.remove()); 
+  [nama, email, pesan].forEach(el => el.style.border = ""); 
+ 
+  let isValid = true; 
