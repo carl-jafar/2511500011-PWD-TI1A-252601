5065fee9165f7352c9e1c77e7703b7a1ed9fe2c5
Muhammad Haikal <2511500011@mahasiswa.atmaluhur.ac.id>
2025-11-19 12:28:32 +0700

    baru

--- 2511500011-PWD-TI1A-252601/pertemuan-07/get_proses.php
+++
@@ -1,7 +0,0 @@
-<?php 
-session_start();
-  $sesname = $_SESSION["nama"] = $_POST["txtNama"]
-  $sesemail = $_SESSION["email"] = $_POST["txtEmail"]
-  $sespesan = $_SESSION["pesan"]= $_POST["txtPesan"]
-  echo $_SESSIONl.["nama"].$_SESSION["email"].$_SESSION["pesan"]  
-?>
