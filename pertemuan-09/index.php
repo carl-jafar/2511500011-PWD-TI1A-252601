cd119fa64dde5c59b5888c8c7690555d5ef34f8c
Muhammad Haikal <2511500011@mahasiswa.atmaluhur.ac.id>
2025-11-29 20:52:38 +0700

    menambahkan htmlspecialchars, digunakan untuk input yang berasal dari pengguna

--- pertemuan-09/index.php
+++ pertemuan-09/index.php
@@ -16,55 +16,6 @@ if (isset($_SESSION["sespesan"])):
   $sespesan = $_SESSION["sespesan"];
 endif;
 
-$txtNim = "";
-if (isset($_SESSION["txtNim"])):
-  $txtNim = $_SESSION["txtNim"];
-endif;
-
-$txtNmLengkap = "";
-if (isset($_SESSION["txtNmLengkap"])):
-  $txtNmLengkap = $_SESSION["txtNmLengkap"];
-endif;
-
-$txtT4Lhr = "";
-if (isset($_SESSION["txtT4Lhr"])):
-  $txtT4Lhr = $_SESSION["txtT4Lhr"];
-endif;
-
-$txtTglLhr = "";
-if (isset($_SESSION["txtTglLhr"])):
-  $txtTglLhr = $_SESSION["txtTglLhr"];
-endif;
-
-$txtHobi = "";
-if (isset($_SESSION["txtHobi"])):
-  $txtHobi = $_SESSION["txtHobi"];
-endif;
-
-$txtPasangan = "";
-if (isset($_SESSION["txtPasangan"])):
-  $txtPasangan = $_SESSION["txtPasangan"];
-endif;
-
-$txtKerja = "";
-if (isset($_SESSION["txtKerja"])):
-  $txtKerja = $_SESSION["txtKerja"];
-endif;
-
-$txtNmOrtu = "";
-if (isset($_SESSION["txtNmOrtu"])):
-  $txtNmOrtu = $_SESSION["txtNmOrtu"];
-endif;
-
-$txtNmKakak = "";
-if (isset($_SESSION["txtNmKakak"])):
-  $txtNmKakak = $_SESSION["txtNmKakak"];
-endif;
-
-$txtNmAdik = "";
-if (isset($_SESSION["txtNmAdik"])):
-  $txtNmAdik = $_SESSION["txtNmAdik"];
-endif;
 ?>
 
 <!DOCTYPE html>
@@ -153,6 +104,13 @@ endif;
     </section>
 
     <section id="about">
+      <?php foreach ($fieldcongif as $key => $metadata): ?>
+      <p>
+        <strong><?= $metadata["label"] ?></strong>
+        <?= htmlspecialchars(string: $biodata[$kunci] ?? "") ?>
+        <?= $metadata["suffix"] ?>
+      </p>
+
       <h2>Tentang Saya</h2>
       <p><strong>NIM:</strong> <?= $txtNim ?></p>
       <p><strong>Nama Lengkap:</strong> <?= $txtNmLengkap ?> &#128526;</p>
