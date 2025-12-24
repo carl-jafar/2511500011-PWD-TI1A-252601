33a66546e8a6bd85e0ab0797aa4116e7acd60542
Muhammad Haikal <2511500011@mahasiswa.atmaluhur.ac.id>
2025-12-24 11:18:40 +0700

    import sql bahan praktikum pertemuan 13

--- pertemuan-13/koneksi.php
+++ pertemuan-13/koneksi.php
@@ -1,12 +1,11 @@
-<?php 
+<?php
 $host = "localhost";
 $user = "root";
 $pass = "root";
-$db = "db_pwd2025";
+$db   = "db_pwd2025";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-    die("koneksi gagal:" . mysqli_connect_error());
+  die("Koneksi gagal: " . mysqli_connect_error());
 }
-?>
---
+++ pertemuan-13/readme.md
@@ -0,0 +1,11 @@
+# pertemuan-13
+
+hari ini tanggal 24 desember 2025 saya bernama muhammad haikal
+nim:2511500011
+
+<ol>
+  <li>import sql bahan praktikum pertemuan 13</li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ol>
